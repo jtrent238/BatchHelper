cad9aed60a2c4835f43e43ba5d8aaf14003fb679
Trent Patterson <jtrent238@outlook.com>
2016-12-22 18:39:56 -0500

    Update BatchHelper.bat

--- BatchHelper/BatchHelper.bat
+++ BatchHelper/BatchHelper.bat
@@ -1,6 +1,16 @@
 @echo off
 cls
 title BatchHelper
-start BatchDebug.bat
-RandomNumber.bat
+:start BatchDebug.bat
+:RandomNumber.bat
+
+echo -----------------------------------------------------------
+echo >>> Batch Helper Current Commands <<<
+echo -----------------------------------------------------------
+echo /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+echo -----------------------------------------------------------
+echo RandomNumber -- Gives you a random number.
+echo RandomNumberHelp -- Displays help for the above command.
+echo RandomNumberLog -- Opens a log file for the generator.
+echo -----------------------------------------------------------
 pause
