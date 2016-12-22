4276777867c7749e5b0950d985d6b64b8d8d9aa9
Trent Patterson <jtrent238@outlook.com>
2016-12-22 18:46:15 -0500

    Add files via upload

---
+++ BatchHelper/BHelp.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+title BatchHelper - Help
+
+echo -----------------------------------------------------------
+echo >>> Batch Helper Current Commands <<<
+echo -----------------------------------------------------------
+echo /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+echo -----------------------------------------------------------
+echo RandomNumber -- Gives you a random number.
+echo RandomNumberHelp -- Displays help for the above command.
+echo RandomNumberLog -- Opens a log file for the generator.
+echo RandomNumberLog -- Opens a log file for the generator.
+echo BHelp -- Opens BatchHelper Help.
+echo -----------------------------------------------------------
+pause
--- BatchHelper/BatchHelper.bat
+++ BatchHelper/BatchHelper.bat
@@ -4,13 +4,10 @@ title BatchHelper
 :start BatchDebug.bat
 :RandomNumber.bat
 
-echo -----------------------------------------------------------
-echo >>> Batch Helper Current Commands <<<
-echo -----------------------------------------------------------
 echo /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
 echo -----------------------------------------------------------
-echo RandomNumber -- Gives you a random number.
-echo RandomNumberHelp -- Displays help for the above command.
-echo RandomNumberLog -- Opens a log file for the generator.
+echo >>> Use Command 'BHelper' For all commands. <<<
 echo -----------------------------------------------------------
+echo /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
 pause
